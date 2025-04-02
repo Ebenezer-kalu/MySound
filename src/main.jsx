6f54fd6473b01a3a33a9b7100f3abcb28cb6caa6
Ebenezer Kalu <ebenezerkalu01@gmail.com>
2025-04-02 14:46:20 +0100

    Saving my local changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";  // ✅ Only here
 import App from "./App";
-import ErrorBoundary from './ErrorBoundary';
+import ErrorBoundary from "./ErrorBoundary";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
