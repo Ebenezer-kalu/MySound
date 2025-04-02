f550426e39402a69a08fe0d256248abfc172562b
Ebenezer Kalu <ebenezerkalu01@gmail.com>
2025-04-02 15:02:35 +0100

    Fix missing file issue

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";  // ✅ Only here
 import App from "./App";
-import ErrorBoundary from "./ErrorBoundary";
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>  {/* ✅ Wrap the entire app here */}
-    <ErrorBoundary>
+    
       <App />
-    </ErrorBoundary>
+    
     </BrowserRouter>
   </React.StrictMode>
 );
